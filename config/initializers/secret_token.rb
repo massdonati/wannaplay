4c0e0dc299ac4daa90ffa8efb3904ea8c5d3704a
Massimo Donati <mass.donati@gmail.com>
2012-07-22 16:46:33 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WannaPlay::Application.config.secret_token = 'e180fd2c87b4b7307a71dcdfffd0c90fb254652c969858a3102fd922266832657aaf21a212a29e4f0df8af0c0b402f7cb628b4fc440499351b8ea7edf93b381e'
