ac983aac11581a0330b4534d33f796bba73f5670
Massimo Donati <mass.donati@gmail.com>
2013-10-15 21:46:37 -0700

    Added Admin field to User model and other minor changes
    Updated to rails 3.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User
   field :last_sign_in_at,    :type => Time
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
+  field :admin,              :type => Boolean, :default => false
 
   ## Confirmable
   # field :confirmation_token,   :type => String
